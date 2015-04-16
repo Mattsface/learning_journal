dbfbd707a41f710e4d8e745c2a1b85ac00685055
MattsFace <spahmatthew@gmail.com>
2015-04-15 19:18:34 -0700

    Added the User model

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -58,4 +58,20 @@ class Entry(Base):
         return session.query(cls).order_by(sa.desc(cls.created)).all()
 
 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(Unicode(length=255), unique=True, nullable=False)
+    password = Column(Unicode(), nullable=True)
+    @classmethod
+    def by_user(cls, username, session=None):
+        """
+        return entry an entry based on its index id, if not found return None
+        """
+        if session is None:
+            session = DBSession
+        return session.query(cls).filter(cls.username==username).first()
+
+
+
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
